1d67713b62184594e4fdbdfba837a9d4b6a0d128
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-09 22:55:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  for(driver of drivers){
+    console.log(driver.name);
+  }
+}
+
+function logDriversByHometown(drivers, location){
+  drivers.filter(driver => driver.hometown === location).forEach(driver => console.log(driver.name));
+}
+
+function driversByRevenue(drivers){
+  let newDrivers = [...drivers]
+  return newDrivers.sort((a, b) => a.revenue - b.revenue);
+}
+
+function driversByName(drivers){
+  let newDrivers = [...drivers]
+  return newDrivers.sort((a, b) => a.name > b.name);
+};
+
+function totalRevenue(drivers){
+  let newDrivers = [...drivers]
+  return newDrivers.reduce((acc, driver) => acc + driver.revenue, 0);
+}
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers)/drivers.length
+}
